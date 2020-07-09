269d35f438aaa204a1116baa0594e2dd3640aa61
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-07-09 12:48:23 -0500

    [IMP] get now in FTP

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'OneBeat integration',
-    'version': '12.0.1.1.0',
+    'version': '12.0.1.2.0',
     'author': 'Humanytek',
     'depends': [
         'stock',
