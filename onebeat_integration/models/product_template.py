d10ec92c357c688fb99fc117e5caccf7c32e67cd
Moisés Navarro <moisalejandro@gmail.com>
2022-01-05 16:53:26 -0600

    Revert "[ADD] name and default_code restrictions"
    This reverts commit f5b5cb6472a5eefd1667b00fc1ebcbfbd664e7df.

--- onebeat_integration/models/product_template.py
+++ onebeat_integration/models/product_template.py
@@ -1,28 +1,7 @@
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     buffer_size = fields.Float()
-    default_code = fields.Char(  # Inherit base field
-        required=True,
-    )
-
-    _sql_constraints = [
-        (
-            "uniq_default_code",
-            "UNIQUE(default_code, company_id)",
-            _("The default code must be unique per company."),
-        )
-    ]
-
-    @api.constrains("name")
-    def _check_valid_name(self):
-        invalid_chars = ",'\""
-        for record in self:
-            if any(char in record.name for char in invalid_chars):
-                raise ValueError(
-                    _("The name of the product can not contain the following characters: %s")
-                    % invalid_chars
-                )
