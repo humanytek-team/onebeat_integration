2ff400f87fab3c424b13a26fdbba664a1f16f8db
Moises Navarro <moisalejandro@gmail.com>
2023-09-06 13:09:31 -0600

    [ADD] unique_product_code

---
+++ unique_product_code/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Unique Product Code",
+    "version": "13.0.0.1.0",
+    "author": "Humanytek",
+    "license": "LGPL-3",
+    "depends": [
+        "product"
+    ],
+    "data": [
+        # security
+        # data
+        # reports
+        # views
+    ],
+}
