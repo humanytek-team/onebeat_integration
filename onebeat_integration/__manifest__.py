121414ad73bfe5fda22b3ac4384013b57e4cf16b
Moises Navarro <moisalejandro@gmail.com>
2024-03-26 15:03:29 -0600

    feat(version): Migrate to 17.0

--- onebeat_integration/__manifest__.py
+++ onebeat_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "OneBeat integration",
-    "version": "14.0.1.0.0",
+    "version": "17.0.0.1.0",
     "author": "Humanytek",
     "license": "LGPL-3",
     "depends": [
--- unique_product_code/__manifest__.py
+++ unique_product_code/__manifest__.py
@@ -1,11 +1,9 @@
 {
     "name": "Unique Product Code",
-    "version": "13.0.0.1.0",
+    "version": "17.0.0.1.0",
     "author": "Humanytek",
     "license": "LGPL-3",
-    "depends": [
-        "product"
-    ],
+    "depends": ["product"],
     "data": [
         # security
         # data
